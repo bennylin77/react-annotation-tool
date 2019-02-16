52cf7e0f866e2d903918d6badfb718a3f14b664e
root <root@ivc.ischool.utexas.edu>
2019-02-15 23:12:35 -0600

    button changes

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path");
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
@@ -15,9 +16,13 @@ module.exports = merge(common, {
   },
   devServer: {
     contentBase: path.join(__dirname, "public/"),
-		host: 'www.chi-lin.com',
-    port: 3000,
-    publicPath: "http://www.chi-lin.com:3000/dist/",
+		host: 'ivc.ischool.utexas.edu',
+      https: {
+	  key: fs.readFileSync("/etc/ssl/apache2/ivc.key"),
+	  cert: fs.readFileSync("/etc/ssl/apache2/ivc.crt"),
+      },
+    port: 8080,
+    publicPath: "ivc.ischool.utexas.edu:8080/dist/",
     hotOnly: true
   },
   plugins: [
