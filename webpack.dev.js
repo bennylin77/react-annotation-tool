9cdcebf2d8e64c802a9a0b77bc0d16b012ff8fd8
Benny Lin <benny@houzz.com>
2019-03-24 18:47:11 -0700

    set webpack.dev.js to use .env constant

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,27 +1,26 @@
-//require('dotenv').config()
-const path = require("path");
+require('dotenv').config();
+const path = require('path');
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 const common = require('./webpack.common.js');
-const webpack = require("webpack");
 
 module.exports = merge(common, {
-  mode: "development",
-	devtool: 'eval',
-  entry: "./src/demo/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: 'demo.bundle.js',
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-	host: "ivc.ischool.utexas.edu",
-      https:true,
-      port: 8082,
-    publicPath: process.env.PUBLIC_PATH,
-    hotOnly: true
-  },
-  plugins: [
-		new webpack.HotModuleReplacementPlugin()
-	]
+    mode: 'development',
+    devtool: 'eval',
+    entry:   './src/demo/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: 'demo.bundle.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        host: process.env.HOST,
+        port: process.env.PORT,
+        publicPath: process.env.PUBLIC_PATH,
+        hotOnly: true,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
 });
